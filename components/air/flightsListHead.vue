d55199b72eaedba862b29631e97e1b183461ab06
lily <943165920@qq.com>
2019-09-07 16:29:38 +0800

    13-实现机票列表页的头部组件样式

---
+++ components/air/flightsListHead.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <el-row class="flight-title">
+      <el-col :span="5">航空信息</el-col>
+      <el-col :span="14">
+        <el-row type="flex" justify>
+          <el-col :span="12">起飞时间</el-col>
+          <el-col :span="12">到达时间</el-col>
+        </el-row>
+      </el-col>
+      <el-col :span="5">价格</el-col>
+    </el-row>
+  </section>
+</template>
+<script>
+export default {};
+</script>
+<style lang="less" scoped>
+.flight-title {
+    text-align: center;
+    font-size: 12px;
+    height: 38px;
+    border: 1px solid #ddd;
+    padding: 0 15px;
+    line-height: 38px;
+    margin-bottom: 10px;
+    background-color: #f6f6f6;
+    color: #666;
+}
+</style>
---
+++ pages/air/flights.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="flights">
+        <el-row type="flex" justify="space-between" >
+          <div class="flights-content">
+            <!-- 过滤条件 -->
+            <div></div>
+
+            <!-- 头部布局 -->
+            <FlightsListHead />
+
+            <!-- 航班信息 -->
+            <div></div>
+           
+          </div>
+
+          <!-- 右侧栏布局 -->
+          <div class="aside"></div>
+        </el-row>
+    </section>
+</template>
+<script>
+import FlightsListHead from '@/components/air/flightsListHead'
+export default {
+    components : {
+      FlightsListHead
+    }
+}
+</script>
+<style lang="less" scoped>
+.flights {
+  // min-width: 1000px;
+  width: 1000px;
+  margin: 0 auto;
+}
+.flights-content {
+  width: 745px;
+}
+
+</style>
