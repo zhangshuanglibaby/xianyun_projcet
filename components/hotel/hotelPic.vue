482af72379fdc51a994c43291dd0bae40032e674
lily <943165920@qq.com>
2019-09-14 16:09:25 +0800

    37-实现酒店页面详情的图片切换功能

--- components/hotel/hotelPic.vue
+++ components/hotel/hotelPic.vue
@@ -2,40 +2,15 @@
     <el-row type="flex" justify="" class="hotelPic">
           <el-row type="flex" justify="" class="photo-view">
               <el-col :span="16" class="main-pic">
-                <a href="javascript:;">
-                  <img src="http://157.122.54.189:9093/images/hotel-pics/1.jpeg" alt="漫耕假日酒店(原全季酒店高淳店)">
-                </a>
+                  <img :src="`http://157.122.54.189:9093/images/hotel-pics/${bigPic}.jpeg`" alt="漫耕假日酒店(原全季酒店高淳店)">
               </el-col>
               <el-col :span="8" class="list-pic">
                   <el-row type="flex" justify="" class="list-item">
-                      <el-col :span="12">
+                      <el-col :span="12" v-for="(item,index) in picData" :key="index">
                         <a href="javascript:;">
-                          <img src="http://157.122.54.189:9093/images/hotel-pics/1.jpeg" alt="漫耕假日酒店(原全季酒店高淳店)">
-                        </a>
-                      </el-col>
-                      <el-col :span="12">
-                        <a href="javascript:;">
-                          <img src="http://157.122.54.189:9093/images/hotel-pics/2.jpeg" alt="漫耕假日酒店(原全季酒店高淳店)">
-                        </a>
-                      </el-col>
-                      <el-col :span="12">
-                        <a href="javascript:;">
-                          <img src="http://157.122.54.189:9093/images/hotel-pics/3.jpeg" alt="漫耕假日酒店(原全季酒店高淳店)">
-                        </a>
-                      </el-col>
-                      <el-col :span="12">
-                        <a href="javascript:;">
-                          <img src="http://157.122.54.189:9093/images/hotel-pics/4.jpeg" alt="漫耕假日酒店(原全季酒店高淳店)">
-                        </a>
-                      </el-col>
-                      <el-col :span="12">
-                        <a href="javascript:;">
-                          <img src="http://157.122.54.189:9093/images/hotel-pics/5.jpeg" alt="漫耕假日酒店(原全季酒店高淳店)">
-                        </a>
-                      </el-col>
-                      <el-col :span="12">
-                        <a href="javascript:;">
-                          <img src="http://157.122.54.189:9093/images/hotel-pics/6.jpeg" alt="漫耕假日酒店(原全季酒店高淳店)">
+                          <img :src="`http://157.122.54.189:9093/images/hotel-pics/${item}.jpeg`" 
+                          alt="漫耕假日酒店(原全季酒店高淳店)" 
+                          @click="handleClick(item)">
                         </a>
                       </el-col>
                   </el-row>
@@ -46,7 +21,18 @@
 
 <script>
 export default {
-
+  data() {
+    return {
+      bigPic : '1',
+      picData : [1,2,3,4,5,6]
+    }
+  },
+  methods : {
+    handleClick(item) {
+      // console.log(item)
+      this.bigPic = item
+    }
+  }
 };
 </script>
 
